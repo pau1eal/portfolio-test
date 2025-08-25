561b14488255cb2df8f3f9f73a0fb11d051db79d
pau1eal <135766674+pau1eal@users.noreply.github.com>
2025-08-26 00:13:20 +0200

    Update artstation.js

--- api/artstation.js
+++ api/artstation.js
@@ -1,15 +1,14 @@
-// api/artstation.js
-export default async function handler(req, res) {
-  const USER = 'pauleal';
-  const URL = `https://www.artstation.com/users/${USER}/projects.json`;
-  
-  try {
-    const response = await fetch(URL); // fetch nativo de Node 18+
-    if (!response.ok) throw new Error(`HTTP ${response.status}`);
-    const data = await response.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
+export default async function handler(req,res){
+  const USER='pauleal';
+  const URL=`https://www.artstation.com/users/${USER}/projects.json`;
+
+  try{
+    const response=await fetch(URL);
+    if(!response.ok) throw new Error(`HTTP ${response.status}`);
+    const data=await response.json();
+    res.setHeader('Access-Control-Allow-Origin','*');
     res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  }catch(err){
+    res.status(500).json({error:err.message});
   }
 }
